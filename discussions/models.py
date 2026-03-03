0b78884e81be5e65aecedbdbc81637c8d9f73932
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-03-03 17:02:44 +0100

    feat: add Discussion model

--- discussions/models.py
+++ discussions/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+class Discussion(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="discussions"
+    )
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
