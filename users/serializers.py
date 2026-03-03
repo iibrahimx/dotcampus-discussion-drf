b1725addcc4a64d87f13061b13ef3cc1ad0f1bd1
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-03-03 15:09:27 +0100

    feat: add user signup endpoint

---
+++ users/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import User
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password"]
+
+    def create(self, validated_data):
+        return User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data.get("email", ""),
+            password=validated_data["password"],
+        )
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import SignupSerializer
+
 
 # Create your views here.
+class SignupView(generics.CreateAPIView):
+    serializer_class = SignupSerializer
+    permission_classes = [permissions.AllowAny]
